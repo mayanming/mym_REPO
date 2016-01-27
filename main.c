42074a972f684e33f7ac72f37ba93cf9d31e3f0c
mym <1414781583@qq.com>
2016-01-27 17:59:53 +0800

    main.c提交

---
+++ main.c
@@ -0,0 +1,18 @@
+/***************************************************************************************
+    >File Name: main.c
+    >Author: mym
+    >Mail: 1367979155@qq.com
+    >Created Time: 2016年01月27日 星期三 17时38分23秒
+ ******************************************************************************/
+
+#include <stdio.h>
+
+
+int main(int argc,char *argv[])
+{
+zaaaaaaa
+	printf("hello world\n");
+	getchar();
+    return 0;
+}
+
